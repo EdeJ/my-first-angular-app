34c0898d7dbec9a435314eb8c05a4eb803c7334f
Emiel de Jong <emieldejong@xs4all.nl>
2021-03-04 16:51:37 +0100

    assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,41 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
-  styleUrls: ['./servers.component.css']
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TEST1';
+  userName = '';
+  buttonDisabled = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetUserName() {
+    this.userName = '';
+    this.buttonDisabled = true;
+  }
+
+  updateUserName(event) {
+    if (this.userName != '') {
+      this.buttonDisabled = false;
+    }
+  }
 }
